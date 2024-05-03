c897108550794fdac00c890bfbcc6a5b1cc51a37
GravitySucks <fabiobassani@MacBook-Air-di-Fabio.local>
2024-05-03 14:22:44 +0200

    added new function and optimized code

--- black_scholes.py
+++ black_scholes.py
@@ -1,55 +1,22 @@
-# GRAVITY SUCKS
-# Valuation of European options in Black-Scholes-Merton model
-# and their relative greeks, and implied volatility estimator
-# in object-oriented programmed language
-
 from math import log, sqrt, exp
 from scipy.stats import norm
 
-class BSM():
-    def __init__(self, S, K, r, T, option_type, sigma=0, option_price = 0):
+class BlackScholes():
+    def __init__(self, S, K, r, T, option_type, sigma=0, option_price=0):
         ''' Black-Scholes Used for pricing European options on stocks without dividends.
         Attributes
         ==========
-        S0: float
-            initial stock/index level
-        K: float
-            strike price
-        T: float
-            maturity (in year fractions)
-        r: float
-            constant risk-free short rate
-        option_type: string
-            'put' or 'call'
-        sigma: float
-            volatility factor in percentage term
-        option_price: float
-            price of option     
-        
-        
-        e.g.
-        
-        Given implied volatility return the option_price
-        BSM(S, K, r, T, option_type, sigma =sigma).price()
-        
-        Other methods:
-        - d1
-        - d2
-        - delta
-        - gamma
-        - theta
-        - vega
-        
-        
-        Given an option price return the implied volatility
-        BSM(S, K, r, T, option_type, option_price = option_price).implied_volatility()
-        
-        
+        S: spot/underlying price
+        K: strike price
+        T: time to expiration (in year)
+        r: risk-free rate
+        option_type: 'put'/'call'
+        sigma: implied volatility percentage
+        option_price: price of option     
+
+        User can alternatevely uses option_price or sigma as inputs.
         
         '''
-        
-        
-    
         if sigma == 0 and option_price == 0:
             raise ValueError('Sigma or option_price must be used to initialize the class')
     
@@ -57,22 +24,31 @@ class BSM():
         self.K = K
         self.r = r
         self.T = T
-        self.option_type = option_type
+        self.option_type = option_type.lower()
         self.sigma = sigma
         self.option_price = option_price
+    
+    def _ensure_sigma_initialized(self):
+        if self.sigma == 0:
+            self.sigma = self.implied_volatility()
+    
+    def _ensure_price_initialized(self):
+        if self.option_price == 0:
+            self.option_price = self.price()
 
     def d1(self):
+        self._ensure_sigma_initialized()
         d1 = (log(self.S/self.K) + (self.r + self.sigma**2/2)*self.T) / (self.sigma * sqrt(self.T))
         return d1
 
     def d2(self):
-        d1 = (log(self.S/self.K) + (self.r + self.sigma**2/2)*self.T) / (self.sigma* sqrt(self.T))
+        self._ensure_sigma_initialized()
+        d1 = self.d1()
         d2 = d1 - self.sigma * sqrt(self.T)
         return d2
 
     def price(self):
-        ''' Returns price of option given implied volatility.
-        '''
+        self._ensure_sigma_initialized()
         if self.option_type == 'call':
             price = self.S * norm.cdf(self.d1()) - self.K * exp(-self.r * self.T) * norm.cdf(self.d2())
         elif self.option_type == 'put':
@@ -101,17 +77,25 @@ class BSM():
             theta_calc = -self.S*norm.pdf(self.d1())*self.sigma/(2* sqrt(self.T)) + self.r*self.K* exp(-self.r*self.T)*norm.cdf(-self.d2())
         else:
             raise ValueError('Invalid option type')
-        
         return theta_calc / 365
 
     def vega(self):
         vega = self.S * norm.pdf(self.d1()) * sqrt(self.T)
         return vega * 0.01
     
+    def rho(self):
+        self._ensure_sigma_initialized()
+        if self.option_type == 'call':
+            return self.K * self.T * exp(-self.r * self.T) * norm.cdf(self.d2()) /100
+        elif self.option_type == 'put':
+            return -self.K * self.T * exp(-self.r * self.T) * norm.cdf(-self.d2()) /100
+        else:
+            raise ValueError('Invalid option type')
+    
     def implied_volatility(self, sigma_est=0.5, it=100):
-        ''' Returns implied volatility given option price.
-        '''
+        self._ensure_price_initialized()
         option = BSM(self.S, self.K, self.r, self.T, self.option_type, sigma_est)
+        #Newton-Raphson
         for i in range(it):
             option.sigma -= (option.price() - self.option_price) / (option.vega() * 100)
         self.sigma = option.sigma
