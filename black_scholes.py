2c20fcc17b9f6eac51c9721a9a38727bdcc22aa5
GravitySucks <fabiobassani@MacBook-Air-di-Fabio.local>
2024-05-06 17:52:57 +0200

    hidden parameters

--- black_scholes.py
+++ black_scholes.py
@@ -20,83 +20,83 @@ class BlackScholes():
         if sigma == 0 and option_price == 0:
             raise ValueError('Sigma or option_price must be used to initialize the class')
     
-        self.S = S
-        self.K = K
-        self.r = r
-        self.T = T
-        self.option_type = option_type.lower()
-        self.sigma = sigma
-        self.option_price = option_price
+        self._S = S
+        self._K = K
+        self._r = r
+        self._T = T
+        self._option_type = option_type.lower()
+        self._sigma = sigma
+        self._option_price = option_price
     
     def _ensure_sigma_initialized(self):
-        if self.sigma == 0:
-            self.sigma = self.implied_volatility()
+        if self._sigma == 0:
+            self._sigma = self.implied_volatility()
     
     def _ensure_price_initialized(self):
-        if self.option_price == 0:
-            self.option_price = self.price()
+        if self._option_price == 0:
+            self._option_price = self.price()
 
     def d1(self):
         self._ensure_sigma_initialized()
-        d1 = (log(self.S/self.K) + (self.r + self.sigma**2/2)*self.T) / (self.sigma * sqrt(self.T))
+        d1 = (log(self._S/self._K) + (self.r + self._sigma**2/2)*self._T) / (self._sigma * sqrt(self._T))
         return d1
 
     def d2(self):
         self._ensure_sigma_initialized()
         d1 = self.d1()
-        d2 = d1 - self.sigma * sqrt(self.T)
+        d2 = d1 - self._sigma * sqrt(self._T)
         return d2
 
     def price(self):
         self._ensure_sigma_initialized()
-        if self.option_type == 'call':
-            price = self.S * norm.cdf(self.d1()) - self.K * exp(-self.r * self.T) * norm.cdf(self.d2())
-        elif self.option_type == 'put':
-            price = self.K * exp(-self.r * self.T) * norm.cdf(-self.d2()) - self.S * norm.cdf(-self.d1())
+        if self._option_type == 'call':
+            price = self._S * norm.cdf(self.d1()) - self._K * exp(-self.r * self._T) * norm.cdf(self.d2())
+        elif self._option_type == 'put':
+            price = self._K * exp(-self.r * self._T) * norm.cdf(-self.d2()) - self._S * norm.cdf(-self.d1())
         else:
             raise ValueError('Invalid option type')
         return price
     
     def delta(self):
-        if self.option_type == 'call':
+        if self._option_type == 'call':
             delta = norm.cdf(self.d1())
-        elif self.option_type == 'put':
+        elif self._option_type == 'put':
             delta = -norm.cdf(-self.d1())
         else:
             raise ValueError('Invalid option type')
         return delta
         
     def gamma(self):
-        gamma = norm.pdf(self.d1()) / (self.S*self.sigma* sqrt(self.T))
+        gamma = norm.pdf(self.d1()) / (self._S*self._sigma* sqrt(self._T))
         return gamma
 
     def theta(self):
-        if self.option_type == 'call':
-            theta_calc = -self.S*norm.pdf(self.d1())*self.sigma/(2* sqrt(self.T)) - self.r*self.K* exp(-self.r*self.T)*norm.cdf(self.d2())
-        elif self.option_type == 'put':
-            theta_calc = -self.S*norm.pdf(self.d1())*self.sigma/(2* sqrt(self.T)) + self.r*self.K* exp(-self.r*self.T)*norm.cdf(-self.d2())
+        if self._option_type == 'call':
+            theta_calc = -self._S*norm.pdf(self.d1())*self._sigma/(2* sqrt(self._T)) - self._r*self._K* exp(-self.r*self._T)*norm.cdf(self.d2())
+        elif self._option_type == 'put':
+            theta_calc = -self._S*norm.pdf(self.d1())*self._sigma/(2* sqrt(self._T)) + self._r*self._K* exp(-self.r*self._T)*norm.cdf(-self.d2())
         else:
             raise ValueError('Invalid option type')
         return theta_calc / 365
 
     def vega(self):
-        vega = self.S * norm.pdf(self.d1()) * sqrt(self.T)
+        vega = self._S * norm.pdf(self.d1()) * sqrt(self._T)
         return vega * 0.01
     
     def rho(self):
         self._ensure_sigma_initialized()
-        if self.option_type == 'call':
-            return self.K * self.T * exp(-self.r * self.T) * norm.cdf(self.d2()) /100
-        elif self.option_type == 'put':
-            return -self.K * self.T * exp(-self.r * self.T) * norm.cdf(-self.d2()) /100
+        if self._option_type == 'call':
+            return self._K * self._T * exp(-self.r * self._T) * norm.cdf(self.d2()) /100
+        elif self._option_type == 'put':
+            return -self._K * self._T * exp(-self.r * self._T) * norm.cdf(-self.d2()) /100
         else:
             raise ValueError('Invalid option type')
     
     def implied_volatility(self, sigma_est=0.5, it=100):
         self._ensure_price_initialized()
-        option = BlackScholes(self.S, self.K, self.r, self.T, self.option_type, sigma_est)
+        option = BlackScholes(self._S, self._K, self._r, self._T, self._option_type, sigma_est)
         #Newton-Raphson
         for i in range(it):
-            option.sigma -= (option.price() - self.option_price) / (option.vega() * 100)
-        self.sigma = option.sigma
+            option.sigma -= (option.price() - self._option_price) / (option.vega() * 100)
+        self._sigma = option.sigma
         return option.sigma
